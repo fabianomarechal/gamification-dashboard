12289b3c8bccb40a950dc78e812c308a8567fd19
Fabiano Leite <fabiano.marechal@gmail.com>
2024-07-18 14:29:05 -0300

    ajustes

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -7,6 +7,7 @@ import { google } from 'googleapis';
 import { launch } from 'puppeteer-core';
 
 const drive = google.drive({ version: 'v3', auth: oAuthGoogle });
+export const maxDuration = 300;
 
 export async function getUser() {
   const session = await auth();
